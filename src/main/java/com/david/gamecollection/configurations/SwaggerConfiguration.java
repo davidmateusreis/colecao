bb6069cc8deff8c4ca5e650b2d08df2cf0e488d4
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-06-08 17:41:00 -0300

    Criei os métodos Post, Get, Put e Delete.

--- src/main/java/com/david/gamecollection/configurations/SwaggerConfiguration.java
+++ src/main/java/com/david/gamecollection/configurations/SwaggerConfiguration.java
@@ -1,4 +1,39 @@
 package com.david.gamecollection.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
+
+    @Bean
+    public Docket gameListApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+                //.apiInfo(metaInfo());
+    }
+
+    /*private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Game List API REST",
+                "API REST de lista de jogos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("David Mateus", "https://github.com/davidmateusreis",
+                        "davidmateusreis@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+
+        return apiInfo;
+    }*/
 }
